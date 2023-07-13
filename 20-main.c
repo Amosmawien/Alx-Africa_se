65ee2f5b72764bfd7f5095f18185955ae3f91471
amosmawien <mawienmawien84@gmail.com>
2023-07-13 19:16:34 +0300

    dd

---
+++ 20-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+int main ()   
+{  
+  
+int i, num;  
+printf ( " Enter the last number: ");  
+scanf ( " %d", &num);  
+for ( i = 1; i<num; i++)  
+{ 
+
+if ( i == 6 )  
+  
+exit(0);    
+  
+else  
+  
+printf  (" \n Number is %d", i);  
+}   
+return 0;  
+}  
