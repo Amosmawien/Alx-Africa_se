97bcb161b8b8834394319a4b607ffbddb2838995
amosmawien <mawienmawien84@gmail.com>
2023-07-18 19:33:36 +0300

    sse

---
+++ 13-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  
+{  
+typedef unsigned int unit;  
+unit i,j;  
+i=10;  
+j=20;  
+printf("Value of i is :%d",i);  
+printf("\nValue of j is :%d",j);  
+return 0;  
+}  
---
+++ 14-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  
+{  
+typedef unsigned int unit;  
+unit a,b;  
+a=10;  
+b=20;  
+printf("Value of a is :%d",a);  
+printf("\nValue of b is :%d",b);  
+return 0;  
+}  
